7710b4893b86e5bdf1fbead1fa8dca28fe632bf6
Shiro Kawai <shiro@acm.org>
2023-12-25 01:10:12 -1000

    Initial commit

---
+++ app/bsky.scm
@@ -0,0 +1,15 @@
+;;;
+;;; gauche_app_bsky
+;;;
+
+(define-module app.bsky
+  (export test-gauche_app_bsky              ;dummy
+          ))
+(select-module app.bsky)
+
+(define (test-gauche_app_bsky)              ;dummy
+  "gauche_app_bsky is working")
+
+;;
+;; Put your Scheme definitions here
+;;
